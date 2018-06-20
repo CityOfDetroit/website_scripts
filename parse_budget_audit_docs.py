0baf528d50e64aa1980862c484655dd324c30cdf
karlkaebnick <karl.kaebnick@gmail.com>
2018-06-20 14:28:50 -0400

    part-way done on parser

--- parse_budget_audit_docs.py
+++ parse_budget_audit_docs.py
@@ -7,18 +7,51 @@ import requests
 import pdb
 
 
-url = "http://www.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
+def parse_list_item(li):
 
-page = requests.get(url)
-tree = html.fromstring(page.content)
-# reports = tree.xpath('//div[@class="dnntitle"]/text()')
-reports = tree.xpath('//div[@class="dnntitle"]')
 
-rep = reports[0]
+	pdb.set_trace()
 
-dir(rep)
+	link = li.xpath('a')
 
-pdb.set_trace()
 
-elts = rep.xpath('//li')
-elt = elts[0]
+
+def get_item(elt, tag):
+
+	if elt.tag == tag:
+		return elt
+
+	children = elt.getchildren()
+	for child in children:
+
+		item = get_item(child, tag=tag)
+		if item is not None:
+			return item
+
+
+if __name__ == '__main__':
+
+	url = "http://www.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
+
+	page = requests.get(url)
+	tree = html.fromstring(page.content)
+	reports = tree.xpath('//div[@class="dnntitle"]')
+
+	for report in reports:
+
+		subtitle = report.xpath('span')[0].text
+		print(subtitle)
+
+		contents = report.xpath('following-sibling::div[@class="contentmain1"]')
+		for div in contents:
+
+			ul = get_item(div, tag='ul')
+
+			if ul is not None:
+
+				list_items = ul.xpath('li')
+				for li in list_items:
+
+					parse_list_item(li)
+
+					print(list_item.text)
