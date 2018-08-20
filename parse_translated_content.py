fbb5c8109ae5e41c4e4f70165f9bec49b532456b
karlkaebnick <karl.kaebnick@gmail.com>
2018-08-20 14:44:31 -0400

    Clean up script

--- parse_translated_content.py
+++ parse_translated_content.py
@@ -7,39 +7,55 @@ import json
 import glob
 
 
-import pdb
-
-
 class TranslatedContentParser():
+
+    def parse_file(self, filename):
+        """
+        Parse an individual file.
+        """
+
+        print(filename)
+
+        with open(filename) as input:
+
+            # get destination url
+            url = input.readline().rstrip()
+            print(url)
+
+            # skip blank line
+            input.readline()
+
+            # now get the data
+            data = input.readline()
+            data = json.loads(data)
+
+            title = data['name'][0]['value']
+            desc = data['description'][0]['value']
+            summary = data['field_summary'][0]['value']
+
+            self.output_file(title=title, desc=desc, summary=summary)
+
+    def output_file(self, title, desc, summary):
+        """
+        Output the translated content.
+        """
+
+        print("\ntitle:  " + title.rstrip())
+        print("\ndescription:  " + desc.rstrip())
+        print("\nsummary:   " + summary.rstrip())
+        print("\n")
 
     def parse(self, argv):
         """
         Parse all translated files in a given directory / language.
         """
 
-
-        # pdb.set_trace()
-
-
         directory = sys.argv[1]
         lang = sys.argv[2]
 
-        for file in glob.glob(directory + "/*.txt"):
-            print(file)
+        for filename in glob.glob(directory + "/*.txt"):
 
-            with open(file) as input:
-
-                # get destination url
-                url = input.readline().rstrip()
-                print(url)
-
-                # skip blank line
-                input.readline()
-
-                # now get the data
-                data = input.readline()
-                data = json.loads(data)
-                print(type(data))
+            self.parse_file(filename)
 
 
 if __name__ == '__main__':
