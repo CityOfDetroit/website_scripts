9e7b03062db4cf7e14f528d2aedabc6c4d70b327
karlkaebnick <karl.kaebnick@gmail.com>
2018-08-24 17:11:25 -0400

    Mesh all 3 languages together again

--- parse_translated_content.py
+++ parse_translated_content.py
@@ -7,11 +7,17 @@ import json
 import glob
 
 
-class TranslatedContentParser():
+class TranslatedPage():
 
     CLEANUPS = {
         "&lt;": "<",
         "&gt;": ">",
+    }
+
+    LANG_MAP = {
+        "ar": "arabic",
+        "bn": "bengali",
+        "es": "spanish",
     }
 
     def clean_value(self, value):
@@ -40,8 +46,8 @@ class TranslatedContentParser():
         with open(filename) as input:
 
             # get destination url
-            url = input.readline().rstrip()
-            print(url)
+            self.url = input.readline().rstrip()
+            print(self.url)
 
             # skip blank line
             input.readline()
@@ -50,43 +56,73 @@ class TranslatedContentParser():
             data = input.readline()
             data = json.loads(data)
 
-            title = self.parse_value(data, 'name')
-            desc = self.parse_value(data, 'description')
-            summary = self.parse_value(data, 'field_summary')
+            self.title = self.parse_value(data, 'name')
+            self.desc = self.parse_value(data, 'description')
+            self.summary = self.parse_value(data, 'field_summary')
 
-            self.output_file(url=url, title=title, desc=desc, summary=summary)
-
-    def output_file(self, url, title, desc, summary):
+    def output_file(self, url, lang):
         """
         Output the translated content.
         """
 
+        self.output.write("\n" + "language: " + self.LANG_MAP[lang])
         self.output.write("\n" + url)
         self.output.write("\n")
-        self.output.write("\ntitle:  " + title.rstrip())
-        self.output.write("\ndescription:  " + desc.rstrip())
-        self.output.write("\nsummary:   " + summary.rstrip())
+        self.output.write("\ntitle:  " + self.title.rstrip())
+        self.output.write("\ndescription:  " + self.desc.rstrip())
+        self.output.write("\nsummary:   " + self.summary.rstrip())
         self.output.write("\n")
         self.output.write('\n*******************************************************************************\n')
+
+
+class TranslatedContentParser():
+
+    translations = {
+        "ar": {},
+        "bn": {},
+        "es": {},
+    }
+
+    def parse_files(self):
+
+        for lang, directory in self.content.items():
+
+            for filename in glob.glob(directory + "/*.txt"):
+
+                page = TranslatedPage()
+                page.parse_file(filename)
+
+                self.translations[lang][page.url] = page
+
+    def output(self):
+
+        for url in list(ar_content.translations.keys()):
+
+            for lang, page in self.content.items():
+
+                page.output_file(url=self.url, lang=lang)
 
     def parse(self, argv):
         """
         Parse all translated files in a given directory / language.
         """
 
-        directory = sys.argv[1]
-        lang = sys.argv[2]
+        self.content = {
+            "ar" : sys.argv[1],
+            "bn" : sys.argv[2],
+            "es" : sys.argv[3],
+        }
 
-        self.output = open(lang + "_content.txt", newline='', mode='wt', encoding='utf-8')
+        self.output = open("transltated_content.txt", newline='', mode='wt', encoding='utf-8')
 
-        for filename in glob.glob(directory + "/*.txt"):
+        self.parse_files()
 
-            self.parse_file(filename)
+        self.output()
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) is not 3:
-        raise Exception("Usage:  <directory> <language>")
+    if len(sys.argv) is not 4:
+        raise Exception("Usage:  <ar directory> <bn directory> <es directory>")
 
     TranslatedContentParser().parse(sys.argv)
