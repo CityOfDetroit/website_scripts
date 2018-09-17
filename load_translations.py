c4cdd61204832dc79d2171a932d69672f135647d
karlkaebnick <karl.kaebnick@gmail.com>
2018-09-17 13:48:32 -0400

    Translations Loader - move everything into a class

--- load_translations.py
+++ load_translations.py
@@ -7,47 +7,70 @@ from sqlalchemy.sql import text
 from util import get_secrets
 
 
-db_info = get_secrets()["DATABASES"]["detroitmi.dev"]
-ssh_host = db_info["SSH_HOST"]
-db_name = db_info["NAME"]
-db_engine = db_info["ENGINE"]
-db_user = db_info["USER"]
-db_pass = db_info["PASSWORD"]
+class TranslationsLoader():
 
+    db_info = get_secrets()["DATABASES"]["detroitmi.dev"]
+    ssh_host = db_info["SSH_HOST"]
+    db_name = db_info["NAME"]
+    db_engine = db_info["ENGINE"]
+    db_user = db_info["USER"]
+    db_pass = db_info["PASSWORD"]
 
-server = SSHTunnelForwarder(
-        ssh_address_or_host=ssh_host,
-        ssh_username='detroitmi.dev',
-        ssh_pkey='~/.ssh/id_rsa',
-        remote_bind_address=('127.0.0.1', 3306),
-        logger=create_logger(loglevel=0))
+    def start(self):
+        """
+        Connect to server (ssh as well as database).
+        """
 
-server.start()
+        self.server = SSHTunnelForwarder(
+                ssh_address_or_host=self.ssh_host,
+                ssh_username='detroitmi.dev',
+                ssh_pkey='~/.ssh/id_rsa',
+                remote_bind_address=('127.0.0.1', 3306),
+                logger=create_logger(loglevel=0))
 
-local_port=str(server.local_bind_port)
+        self.server.start()
 
+        local_port=str(self.server.local_bind_port)
 
-engine = create_engine('{}://{}:{}@127.0.0.1:{}/{}'.format(db_engine, db_user, db_pass, local_port, db_name))
-conn = engine.connect()
+        self.engine = create_engine('{}://{}:{}@127.0.0.1:{}/{}'.format(self.db_engine, self.db_user, self.db_pass, local_port, self.db_name))
+        self.conn = self.engine.connect()
 
-# tables = engine.table_names()
-# print("num tables:  {}".format(len(tables)))
+    def test(self):
+        """
+        Do sql stuff.
+        """
 
-sql = text(" \
-select tth.tid, tfd.name \
-from taxonomy_term_hierarchy tth \
-join taxonomy_term_field_data tfd \
-on tth.tid = tfd.tid \
-where tfd.langcode = 'en' \
-order by tfd.name; \
-")
+        # tables = engine.table_names()
+        # print("num tables:  {}".format(len(tables)))
 
-results = conn.execute(sql).fetchall()
+        sql = text(" \
+            select tth.tid, tfd.name \
+            from taxonomy_term_hierarchy tth \
+            join taxonomy_term_field_data tfd \
+            on tth.tid = tfd.tid \
+            where tfd.langcode = 'en' \
+            order by tfd.name; \
+            ")
 
-for row in results:
-	print(row)
+        results = self.conn.execute(sql).fetchall()
 
-conn.close()
-engine.dispose()
+        for row in results:
+        	print(row)
 
-server.stop()
+    def stop(self):
+        """
+        Disconnect and stop server and database connections.
+        """
+
+        self.conn.close()
+        self.engine.dispose()
+
+        self.server.stop()
+
+if __name__ == '__main__':
+
+    loader = TranslationsLoader()
+
+    loader.start()
+    loader.test()
+    loader.stop()
