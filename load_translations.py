3c7c93c6291d20b79253f3d96945bbf1853cbb87
karlkaebnick <karl.kaebnick@gmail.com>
2018-09-18 10:39:12 -0400

    Translations Loader:  parse command line better

--- load_translations.py
+++ load_translations.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
+import sys
+
 from parse_translated_content import TranslatedPage
 from translations_loader import TranslationsLoader
 
 
 if __name__ == '__main__':
+
+    # Usage:  <language> <filename>
+    # e.g., load_translations.py es translated_content/es/%2Fdepartments%2Fdetroit-fire-department%2Ffire-investigation-division%2Farson-awareness9.txt
+
+    if len(sys.argv) != 3:
+        raise Exception("Usage:  <language> <filename>")
+
+    lang = sys.argv[1]
+    filename = sys.argv[2]
+
+    if '/' + lang + '/' not in filename:
+        raise Exception("Error:  lang {} appears wrong based on file path".format(lang))
 
     loader = TranslationsLoader()
 
@@ -13,8 +27,6 @@ if __name__ == '__main__':
     # loader.table_names()
 
     page = TranslatedPage()
-    filename = "translated_content/es/%2Fdepartments%2Fdetroit-fire-department%2Ffire-investigation-division%2Farson-awareness9.txt"
-    lang = 'es'
     page.parse_file(filename=filename)
 
     loader.load_page(page=page, lang=lang)
