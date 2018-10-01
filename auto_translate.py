6840e2cf11ea786359a57c54f19fe218dfe25bfb
karlkaebnick <karl.kaebnick@gmail.com>
2018-10-01 16:52:38 -0400

    Auto Translate:  translating in all 3 languages now

--- auto_translate.py
+++ auto_translate.py
@@ -13,9 +13,6 @@ import load_translation
 import inspect
 
 
-import pdb
-
-
 def machine_translate(client, lang, text):
 
     # Translate the content
@@ -33,12 +30,22 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
 
+    if url.startswith('http'):
+        url = url[7 : ]
+
+        pos = url.find('/')
+        url = url[pos : ]
+
     # Extract the content to be translated
-    true_url, data = ContentExporter.do_export(url=url, write_to_file=False)
+    tmp = ContentExporter.do_export(url=url, write_to_file=False)
+    if not tmp:
 
+        ContentExporter.output_errs = True
+        ContentExporter.do_export(url=url, write_to_file=False)
+        ContentExporter.report_err_cnt()
+        raise Exception('content could not be exported')
 
-    pdb.set_trace()
-
+    true_url, data = tmp
 
     # Instantiate a translation client
     client = translate.Client.from_service_account_json('google_translate_key.json')
@@ -46,20 +53,21 @@ if __name__ == '__main__':
     source_page = TranslatedPage()
     source_page.parse_json(data)
 
-    lang = 'es'
+    for lang in sorted(TranslatedPage.LANG_MAP.keys()):
 
-    # Translate the content
-    translated_page = TranslatedPage()
+        # Translate the content
+        translated_page = TranslatedPage()
 
-    translated_page.vid = source_page.vid
-    translated_page.tid = source_page.tid
+        translated_page.vid = source_page.vid
+        translated_page.tid = source_page.tid
 
-    translated_page.content = machine_translate(client=client, lang=lang, text=source_page.content)
-    translated_page.title =machine_translate(client=client, lang=lang, text=source_page.title)
-    translated_page.desc = machine_translate(client=client, lang=lang, text=source_page.desc)
-    translated_page.organization_head_information = machine_translate(client=client, lang=lang, text=source_page.organization_head_information)
-    translated_page.summary = machine_translate(client=client, lang=lang, text=source_page.summary)
+        translated_page.content = machine_translate(client=client, lang=lang, text=source_page.content)
+        translated_page.title =machine_translate(client=client, lang=lang, text=source_page.title)
+        translated_page.desc = machine_translate(client=client, lang=lang, text=source_page.desc)
+        translated_page.organization_head_information = machine_translate(client=client, lang=lang, text=source_page.organization_head_information)
+        translated_page.summary = machine_translate(client=client, lang=lang, text=source_page.summary)
 
-    # Now write the translated content
-    load_translation.Loader().run_page(lang, translated_page)
-
+        # Now write the translated content
+        load_translation.Loader().run_page(lang, translated_page)
+ 
+    print('\nLoaded all translations')
