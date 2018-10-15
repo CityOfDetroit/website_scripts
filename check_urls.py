37f8915160b587eebb506e5284ed54bd88ee1e72
karlkaebnick <karl.kaebnick@gmail.com>
2018-10-15 17:02:43 -0400

    Update check urls scripts

--- check_urls.py
+++ check_urls.py
@@ -8,29 +8,33 @@ from requests.exceptions import ConnectionError
 if __name__ == '__main__':
 
     filename = "urls.txt"
-    # filename = "urls_to_translate.txt"
     missing_urls = set()
+    missing_urls_list = []
+
+    urls_checked = 0
 
     with open(filename) as file:
         for url in file:
 
+            urls_checked = urls_checked + 1
+            if urls_checked % 25 == 0:
+                print("processed: {} urls".format(urls_checked))
+                sys.stdout.flush()
+
             pos = url.find("detroitmi.gov")
             if pos < 0:
-                break
+                path = url
+            else:
+                path = url[ pos + 13 : ].strip()
 
-            path = url[ pos + 13 : ].strip()
-            url = "http://detroitmi.theneighborhoods.org" + path
+            url = "http://detroitmi.theneighborhoods.org" + path.strip()
 
             try:
                 response = requests.get(url)
                 if response.status_code != 200:
 
                     missing_urls.add(path)
-
-                    count = len(missing_urls)
-                    if count % 10 == 0:
-                        print("percent finished: {}%".format(count / 942 * 100))
-                        sys.stdout.flush()
+                    missing_urls_list.append(path)
 
             except ConnectionError as error:
                 print("fatal error: " + str(error) + " - url: " + url)
@@ -39,5 +43,8 @@ if __name__ == '__main__':
 
     tmp_domain = "detroitmi.theneighborhoods.org"
 
-    for url in sorted(list(missing_urls)):
+    for url in missing_urls_list:
         print("http://" + tmp_domain + url)
+
+    # for url in sorted(list(missing_urls)):
+    #     print("http://" + tmp_domain + url)
