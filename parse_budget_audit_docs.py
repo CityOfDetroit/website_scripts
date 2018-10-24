60441256dbf60194dc0bbd46d51c7cc8fdb022f4
karlkaebnick <karl.kaebnick@gmail.com>
2018-10-24 14:34:07 -0400

    Parse budget audit docs:  convert to csv

--- parse_budget_audit_docs.py
+++ parse_budget_audit_docs.py
@@ -4,26 +4,15 @@ import csv
 from lxml import html
 import json
 import requests
-
-
-import pdb
+import sys
 
 
 def parse_link(link):
 
-	path = link.get('href')
-	title = link.get('title')
+    path = link.get('href')
+    title = link.get('title')
 
-	pos = path.rfind('/')
-	if pos >= 0:
-		path = path[pos + 1 : ]
-
-	pos = path.find('?')
-	if pos > 0:
-		path = path[ : pos]
-
-	return path, title
-
+    return path, title
 
 def get_child_items(elt, tag):
 
@@ -41,46 +30,64 @@ def get_child_items(elt, tag):
     return items
 
 
+class ReportInfo():
+
+    def __init__(self, section, title, href):
+
+        self.section = section
+        self.report_title = title
+
+        self.href = "https://www.detroitmi.gov" + href
+
+    def get_data(self):
+
+    	return [ self.section, self.report_title, self.href ]
+
+
 if __name__ == '__main__':
 
-	url = "http://www.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
+    url = "http://archive.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
 
-	content = {
-		"base_path": "/sites/detroitmi.localhost/files/migrated_docs/financial-reports/",
-		"reports": {}
-	}
+    content = {
+        "base_path": "/sites/detroitmi.localhost/files/migrated_docs/financial-reports/",
+        "reports": {}
+    }
 
-	page = requests.get(url)
-	tree = html.fromstring(page.content)
-	reports = tree.xpath('//div[@class="dnntitle"]')
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    reports = tree.xpath('//div[@class="dnntitle"]')
+    report_data = []
 
-	for report in reports:
+    for report in reports:
 
-		reports = []
-		contents = report.xpath('following-sibling::div[@class="contentmain1"]')
-		for div in contents:
+        subtitle = report.xpath('span')[0].text
+        contents = report.xpath('following-sibling::div[@class="contentmain1"]')
+        for div in contents:
 
-			uls = get_child_items(elt=div, tag='ul')
-			if uls is None or len(uls) == 0:
+            uls = get_child_items(elt=div, tag='ul')
+            if uls is None or len(uls) == 0:
 
-				links = get_child_items(elt=div, tag='a')
-				if links is not None and len(links):
-					path, title = parse_link(link=links[0])
-					reports.append({ title : path })
+                links = get_child_items(elt=div, tag='a')
+                if links is not None and len(links):
+                    path, title = parse_link(link=links[0])
 
-			else:
+                    report_data.append(ReportInfo(section=subtitle, title=title, href=path))
 
-				list_items = uls[0].xpath('li')
-				for li in list_items:
+            else:
 
-					path, title = parse_link(link=li.xpath('a')[0])
-					reports.append({ title : path })
+                list_items = uls[0].xpath('li')
+                for li in list_items:
 
-		if reports:
-			subtitle = report.xpath('span')[0].text
-			content["reports"][subtitle] = reports
+                    path, title = parse_link(link=li.xpath('a')[0])
 
-	# print(json.dumps(content))
+                    report_data.append(ReportInfo(section=subtitle, title=title, href=path))
 
+    field_names = [ 'Section', 'Report Title', 'URL' ]
 
-	pdb.set_trace()
+    writer = csv.DictWriter(sys.stdout, fieldnames=field_names, quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+
+    for report in report_data:
+
+        row_data = { item[0] : item[1] for item  in zip(field_names, report.get_data()) }
+        writer.writerow(row_data)
