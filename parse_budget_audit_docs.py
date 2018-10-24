eda16ff075c3d3823cfa756234b629d3d5b7d42a
karlkaebnick <karl.kaebnick@gmail.com>
2018-10-24 16:43:45 -0400

    Parse budget audit docs:  cleanup scraper

--- parse_budget_audit_docs.py
+++ parse_budget_audit_docs.py
@@ -1,33 +1,11 @@
 #!/usr/bin/env python
 
 import csv
-from lxml import html
 import json
 import requests
 import sys
 
-
-def parse_link(link):
-
-    path = link.get('href')
-    title = link.get('title')
-
-    return path, title
-
-def get_child_items(elt, tag):
-
-    if elt.tag == tag:
-        return [elt]
-
-    items = []
-    children = elt.getchildren()
-    for child in children:
-
-        tmp = get_child_items(child, tag=tag)
-        if tmp is not None and len(tmp):
-            items.extend(tmp)
-
-    return items
+from bs4 import BeautifulSoup
 
 
 class ReportInfo():
@@ -37,54 +15,55 @@ class ReportInfo():
         self.section = section
         self.report_title = title
 
-        self.href = "https://www.detroitmi.gov" + href
+        href = href.replace(' ', '%20')
+        pos = href.rfind('/')
+        href = href[ pos + 1: ]
+        pos = href.find('?')
+        if pos > 0:
+            href = href[ : pos ]
+
+        self.href = "https://detroitmi.gov/sites/detroitmi.localhost/files/migrated_docs/financial-reports/" + href
 
     def get_data(self):
 
     	return [ self.section, self.report_title, self.href ]
 
+url = "http://archive.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
+dirs = [ "/portals/0/docs/budgetdept/", "/portals/0/docs/finance/" ]
+
+def do_get_report(href):
+
+    for tmp in dirs:
+
+        if tmp in href.lower():
+            return True
+
+    return False
+
 
 if __name__ == '__main__':
 
-    url = "http://archive.detroitmi.gov/How-Do-I/View-City-of-Detroit-Reports/Budget-Audit-and-other-Financial-Reports"
-
-    content = {
-        "base_path": "/sites/detroitmi.localhost/files/migrated_docs/financial-reports/",
-        "reports": {}
-    }
-
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    reports = tree.xpath('//div[@class="dnntitle"]')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     report_data = []
 
-    for report in reports:
+    list_items = soup.find_all('li')
+    for list_item in list_items:
 
-        subtitle = report.xpath('span')[0].text
-        contents = report.xpath('following-sibling::div[@class="contentmain1"]')
-        for div in contents:
+        link = list_item.findChild('a')
+        if link:
 
-            uls = get_child_items(elt=div, tag='ul')
-            if uls is None or len(uls) == 0:
+            href = link.get('href')
+            if do_get_report(href):
 
-                links = get_child_items(elt=div, tag='a')
-                if links is not None and len(links):
-                    path, title = parse_link(link=links[0])
+                span = link.find_previous('div', 'dnntitle')
+                section = span.text.strip()
 
-                    report_data.append(ReportInfo(section=subtitle, title=title, href=path))
-
-            else:
-
-                list_items = uls[0].xpath('li')
-                for li in list_items:
-
-                    path, title = parse_link(link=li.xpath('a')[0])
-
-                    report_data.append(ReportInfo(section=subtitle, title=title, href=path))
+                report_data.append(ReportInfo(section=section, title=link.text.strip(), href=href))
 
     field_names = [ 'Section', 'Report Title', 'URL' ]
 
-    writer = csv.DictWriter(sys.stdout, fieldnames=field_names, quoting=csv.QUOTE_ALL)
+    writer = csv.DictWriter(sys.stdout, fieldnames=field_names, quoting=csv.QUOTE_ALL, dialect=csv.unix_dialect)
     writer.writeheader()
 
     for report in report_data:
