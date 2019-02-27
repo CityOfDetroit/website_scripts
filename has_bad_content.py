5934354f98f9a395c6edda8d09ab40d201cb00a1
karlkaebnick <karl.kaebnick@gmail.com>
2019-02-27 16:45:30 -0500

    Script to find bad content

---
+++ has_bad_content.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        raise Exception("Usage:  has_bad_content.py <file name>")
+
+    filename = sys.argv[1]
+    with open(filename) as file:
+        contents = file.read()
+
+        match = re.search(r'http[s]\:\/[a-zA-Z]', contents)
+
+        code = 1 if match else 0
+        exit(code=code)
