8c5a5229a7bb972d9d75e191650b0c34ff47e13b
karlkaebnick <karl.kaebnick@gmail.com>
2019-03-01 15:42:37 -0500

    Fleshed out has_bad_content.py with more checks

--- has_bad_content.py
+++ has_bad_content.py
@@ -10,11 +10,26 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise Exception("Usage:  has_bad_content.py <file name>")
 
+    PATTERNS = {
+        r'http[s]?\:\/[a-zA-Z]': "Invalid http / https protocol",
+        r'detroitmi.theneighborhoods.org': "Invalid domain: detroitmi.theneighborhoods.org",
+        r'detroitmi.prod.acquia-sites.com': "Invalid domain: detroitmi.prod.acquia-sites.com",
+    }
+
     filename = sys.argv[1]
     with open(filename) as file:
         contents = file.read()
 
-        match = re.search(r'http[s]?\:\/[a-zA-Z]', contents)
+        errors = []
 
-        code = 1 if match else 0
-        exit(code=code)
+        for pattern, description in PATTERNS.items():
+
+            match = re.search(pattern, contents)
+            if match:
+                errors.append(description)
+
+        if errors:
+            print("File {} contains the following errors:  {}".format(filename, ', '.join(errors)))
+            exit(code=1)
+        else:
+            exit(code=0)
